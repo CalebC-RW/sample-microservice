e7f8c629abe40e2613a1a5202c43f229ca92e494
Caleb Ciampaglia <67662318+CalebC-RW@users.noreply.github.com>
2020-11-10 10:24:11 -0500

    Remove javadoc (#2)

--- sample-app/src/main/java/com/robustwealth/microservices/sample/SampleServiceApplication.java
+++ sample-app/src/main/java/com/robustwealth/microservices/sample/SampleServiceApplication.java
@@ -3,12 +3,6 @@ package com.robustwealth.microservices.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Sample Application
- *
- * @author calebciampaglia
- *
- */
 @SpringBootApplication
 public class SampleServiceApplication {
 
