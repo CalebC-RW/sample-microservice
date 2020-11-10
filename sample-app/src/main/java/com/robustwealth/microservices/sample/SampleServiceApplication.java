819b3f5dd1d280768d22f10500836918f6f62dd0
Caleb Ciampaglia <caleb.ciampaglia@robustwealth.com>
2020-11-10 11:26:01 -0500

    Another random commit

--- sample-app/src/main/java/com/robustwealth/microservices/sample/SampleServiceApplication.java
+++ sample-app/src/main/java/com/robustwealth/microservices/sample/SampleServiceApplication.java
@@ -3,6 +3,12 @@ package com.robustwealth.microservices.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Yet another sample service
+ *
+ * @author calebciampaglia
+ *
+ */
 @SpringBootApplication
 public class SampleServiceApplication {
 
